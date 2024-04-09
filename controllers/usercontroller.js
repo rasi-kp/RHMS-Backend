ac364cf53f48a9f198162f79a2dc40c952ac5d40
Rasi K P <rasir239@gmail.com>
2024-04-10 01:20:07 +0530

    error

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -87,7 +87,7 @@ module.exports = {
           return res.status(401).json({ error: "Invalid password" });
         }
         const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '30d' });
-        return res.status(200).json({ token, user: { id: user.id, email: user.email, name: user.name } });
+        return res.status(200).json({ token, role:"patient", user: { id: user.id, email: user.email, name: user.name } });
       } catch (error) {
         console.error('Error logging in:', error);
         return res.status(500).json({ error: "Internal Server Error" });
