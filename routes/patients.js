d23b8f28ec64d86e45ff6d85b7aedfc692d6d5f4
Rasi K P <rasir239@gmail.com>
2024-04-20 00:47:54 +0530

    appointment delete reschedule

--- routes/patients.js
+++ routes/patients.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const isAuth=require('../middleware/isAuth')
 const {dashboard,allp,addp,deletep,editp,editpost,addappointment,
-    alldoctor,alltoken,appointments} = require('../controllers/patientcontroller');
+    alldoctor,alltoken,appointments,cappointment,dappointment,rappointment,} = require('../controllers/patientcontroller');
 const Appointment = require('../model/appointment');
 
 router.get('/dashboard',isAuth,dashboard)
@@ -16,6 +16,9 @@ router.get('/alldoctor',isAuth,alldoctor)
 router.get('/alltokens',isAuth,alltoken)
 router.post('/addappointment',isAuth,addappointment)
 router.get('/appointments',isAuth,appointments)
+router.get('/completeappointment',isAuth,cappointment)
+router.get('/deleteappointment/:id',isAuth,dappointment)
+router.get('/rescheduleappointment',isAuth,rappointment)
 // router.post('/updateProfile',isAuth,updateProfile)
 // router.post('/message',isAuth,message)
 // router.get('/message',isAuth,message_get)
