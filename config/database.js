6b2c513d58d9ea0cb1201b614322a3d310d1986b
Rasi K P <rasir239@gmail.com>
2024-06-05 19:39:02 +0530

    add aws database setup

--- config/database.js
+++ config/database.js
@@ -5,6 +5,13 @@ const databaseUrl = process.env.DATABASE_URL;
 
 const sequelize = new Sequelize(databaseUrl, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you connect to RDS instances with SSL
+      rejectUnauthorized: false // You might want to adjust this based on your SSL certs
+    }
+  },
+  logging: false, // Disable logging; default: console.log
 });
 
 module.exports = { sequelize };
